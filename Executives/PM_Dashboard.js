0ef9b565667e277f3468e806c55ac30596578858
NashTruby <nashtruby66@gmail.com>
2022-11-07 14:08:04 -0500

    message new updates

---
+++ Executives/PM_Dashboard.js
@@ -0,0 +1,42 @@
+// SIDEBAR TOGGLE
+
+var sidebarOpen = false;
+var sidebar = document.getElementById("sidebar");
+
+function openSidebar()
+{
+    if(!sidebarOpen)
+    {
+        sidebar.classList.add("sidebar-responsive");
+        sidebarOpen = true;
+    }
+}
+
+function closeSidebar()
+{
+    if(sidebarOpen)
+    {
+        sidebar.classList.remove("sidebar-responsive");
+        sidebarOpen = false;
+    }
+}
+
+function openDashboard()
+{
+    window.open("PM_Dashboard.html", "_self")
+}
+
+function openManageProperties()
+{
+    window.open("PM_ManageProperties.html", "_self")
+}
+
+function openTenantApplications()
+{
+    window.open("PM_TenantApplications.html", "_self")
+}
+
+function openReports()
+{
+    window.open("PM_Reports.html", "_self")
+}

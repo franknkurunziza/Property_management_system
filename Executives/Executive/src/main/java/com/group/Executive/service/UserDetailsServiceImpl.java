ead2dcb518174f9a9bcac72964633e2e520a9203
Frank Nkurunziza <franknkurunzia@gmail.com>
2022-11-09 21:20:50 -0500

    added properties/applicaion

---
+++ Executives/Executive/src/main/java/com/group/Executive/service/UserDetailsServiceImpl.java
@@ -0,0 +1,32 @@
+package com.group.Executive.service;
+
+
+
+import java.util.Optional;
+
+import com.group.Executive.models.User;
+import com.group.Executive.repository.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepo userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+
+        Optional<User> user = userRepository.findByUserName(userName);
+
+        user.orElseThrow(() -> new UsernameNotFoundException(userName + " not found."));
+
+        return user.map(UserDetailsImpl::new).get();
+    }
+
+}

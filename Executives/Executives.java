24a962f26b237fd99ccb700433f9a604c4c31f3d
NashTruby <nashtruby66@gmail.com>
2022-12-08 19:11:01 -0500

    payment

---
+++ Executives/Executives.java
@@ -0,0 +1,67 @@
+import java.util.List;
+
+public class Executives 
+{
+  public static List propertyList;
+  
+  static boolean login()
+  {
+    return true;
+  }
+
+  static boolean authenticateUser()
+  {
+    return true;
+  }
+
+  static boolean authorizeUser()
+  {
+    return true;
+  }
+
+  static boolean manageProperties()
+  {
+    return true;
+  }
+
+  static boolean makePayments()
+  {
+    return true;
+  }
+
+  static boolean validatePayment()
+  {
+    return true;
+  }
+
+  static boolean approveTenants()
+  {
+    return true;
+  }
+
+  static boolean computePropertyReport()
+  {
+    return true;
+  }
+  
+  static List search()
+  {
+    return propertyList;
+  }
+
+  static boolean encrypt()
+  {
+    return true;
+  }
+
+  static boolean decrypt()
+  {
+    return true;
+  }
+
+  public static void main(String[] args) 
+    {
+      
+      
+    }
+  }
